264d84d3767236fd0c4c253d97bb7812462fa2e2
MihajloESTS <milojevicm374@gmail.com>
2022-05-25 15:08:13 +0200

    test

--- TESTS/test.js
+++ TESTS/test.js
@@ -22,12 +22,14 @@ const schema = mysql.Schema({
 const Grupa = mysql.Model("grupe", schema);
 
 (async function() {
-	let all = await Grupa.find({})
-	console.log(all);
-	console.log(mysql.connection);
-	// await Grupa.create({naziv: "Paket test"});
-	all = (await Grupa.find({}))
-	console.log(all);
+	let a = new Grupa();
+	console.log(a);
+	// let all = await Grupa.find({})
+	// console.log(all);
+	// console.log(mysql.connection);
+	// // await Grupa.create({naziv: "Paket test"});
+	// all = (await Grupa.find({}))
+	// console.log(all);
 	
 	mysql.connection.quit();
 })()
